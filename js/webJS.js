b80aeeffba1b53cf630e0a53820126c3a970946d
Dilruba Showkat <dilrubashowkat@gmail.com>
2017-09-03 13:07:20 -0500

    js function added

--- js/webJS.js
+++ js/webJS.js
@@ -2,3 +2,8 @@ document.write("I was amazed with the password ...");
         var EidDay = "9/2/2017";
         var s = "Was well spent";
         alert(EidDay + " "+s);
+
+function unpredictable(x,y){
+alert("time and love in life is finite"+x+y);
+}
+unpredictable(3,700);
