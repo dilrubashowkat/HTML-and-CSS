e7536ca1bbacfcc999ff8dc9f1f002d039e63e32
Dilruba Showkat <dilrubashowkat@gmail.com>
2017-09-17 18:49:32 -0500

    has() tag used

--- webJquery.js
+++ webJquery.js
@@ -2,6 +2,14 @@ function test(){
 $(document).ready(function(){
 
 $("#p3").hide();
+$("#p3, #p4").hide();
+$("div:first").hide();
+$("div:last").hide();
+$("div:not(#footer,#para9,#imagesideright)").hide();
+$("div:first").hide();
+$("#mainTest p:first").hide();
+$("div:even").hide();
+$("div:odd").hide();
 
 });
 }
