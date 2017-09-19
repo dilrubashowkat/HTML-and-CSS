f879a6e4efc00810cf7b6c7d40b4d77710f2d086
Dilruba Showkat <dilrubashowkat@gmail.com>
2017-09-19 14:33:30 -0500

    html(), text() method explored

--- webJquery.js
+++ webJquery.js
@@ -11,7 +11,8 @@ $("#mainTest p:first").hide();
 $("div:even").hide();
 $("div:odd").hide();
 $("ul li:nth-child(1)").hide();
-
+$("p").html("<h2>buddy the cat with a hat</h2>");
+$("p").text("buddy the cat with a hat");
 
 });
 }
