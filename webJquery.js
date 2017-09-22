4cc0043b1fcf8e672c435c01f465c26a0697e97b
Dilruba Showkat <dilrubashowkat@gmail.com>
2017-09-22 15:23:38 -0500

    replaceWith() explored

--- webJquery.js
+++ webJquery.js
@@ -13,6 +13,10 @@ $("div:odd").hide();
 $("ul li:nth-child(1)").hide();
 $("p").html("<h2>buddy the cat with a hat</h2>");
 $("p").text("buddy the cat with a hat");
-
+$("#para3").css("color","lime").css("border","solid 3px yellow").fadeOut("50000");
+$("p:last").addClass("para");
+$("div").removeClass("sideright");
+$("p:first").toggleClass("p4");
+$("#p3").fadeIn(2500);
 });
 }
